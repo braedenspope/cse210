c2f2a9591ab2ae5764c89564459d51c48004c2ef
braedenspope <braedenspope@hotmail.com>
2023-01-07 13:47:22 -0700

    prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string grade = "";
+        bool pass;
+        Console.Write("What is your grade percentage? ");
+        int percent =  Int32.Parse(Console.ReadLine());
+        if (percent >= 90){
+            grade = "A";
+            pass = true;
+        } else if (percent >= 80) {
+            grade = "B";
+            pass = true;
+        } else if (percent >= 70){
+            grade = "C";
+            pass = true;
+        } else if (percent >= 60){
+            grade = "D";
+            pass = false;
+        } else {
+            grade = "F";
+            pass = false;
+        }
+        if (pass) {
+            Console.Write("You passed!");
+        } else {
+            Console.Write("You failed.");
+        }
+        Console.WriteLine($" Your grade was {grade}.");
     }
 }
