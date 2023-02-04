3243f77b2cf984d1d23c06d6ea0dc2acc9caf2fa
braedenspope <braedenspope@hotmail.com>
2023-02-04 12:46:10 -0700

    changes

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random rand = new Random();
+        int number = rand.Next(1,100);
+        int guess = 0;
+        while (guess != number){
+            Console.Write("\nWhat is your guess? ");
+            guess = Int32.Parse(Console.ReadLine());
+            if (guess > number){
+                Console.WriteLine("\nLower");
+            } else if (guess < number){
+                Console.WriteLine("\nHigher");
+            }
+        }
+        Console.WriteLine("\nYou guessed it!");
     }
 }
